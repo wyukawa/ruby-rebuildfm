7523679926d7fb7eb039bbd31ebf382648ff1124
wyukawa <wyukawa@gmail.com>
2014-03-21 22:33:03 +0900

    parse rss

--- lib/ruby/rebuildfm.rb
+++ lib/ruby/rebuildfm.rb
@@ -1,7 +1,14 @@
 require "ruby/rebuildfm/version"
 
-module Ruby
-  module Rebuildfm
-    # Your code goes here...
+require 'rss'
+
+module Rebuildfm
+  # Your code goes here...
+  def self.run()
+    url = 'http://feeds.rebuild.fm/rebuildfm'
+    rss = RSS::Parser.parse(url)
+    rss.items.each{|item|
+      puts item.title
+    }
   end
 end
